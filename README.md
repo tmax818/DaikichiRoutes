0b26d18684804d40d226808ed9aea4586902c8bc
tmax818 <tylermaxwell661@gmail.com>
2022-09-13 07:11:50 -0700

    windows update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Start a new Spring Starter Project and create a package for your controllers
 
-Create a new controller called DaikichiController, and be sure you annotate it as a @RestController
+Create a new controller called DaikichiController, and be sure you annotate it as a `@RestController`
 
 Have an http GET request to 'http://localhost:8080/daikichi' display text that says 'Welcome!'
 
--- src/main/java/daikichiroutes/DaikichiController.java
+++ src/main/java/daikichiroutes/DaikichiController.java
@@ -10,4 +10,12 @@ public class DaikichiController {
     public String index(){
         return "Welcome";
     }
+    @RequestMapping("/today")
+    public String today(){
+        return "Today you will find luck in all your endeavors!";
+    }    @RequestMapping("/tomorrow")
+    public String tomorrow(){
+        return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+
 }
